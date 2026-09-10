d3454d1e887f8c3756607207164eac29ea21b751
Leon-Achteresch <leon.achteresch@gmail.com>
2026-09-10 17:01:00 +0200

    feat: Add new components and update dependencies for improved UI

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -2,12 +2,22 @@ import { Link, Outlet } from "@tanstack/react-router";
 
 export function RootLayout() {
   return (
-    <>
-      <nav className="row nav">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
+    <div className="min-h-screen bg-neutral-100 text-neutral-900 dark:bg-neutral-800 dark:text-neutral-100">
+      <nav className="flex justify-center gap-4 p-4">
+        <Link
+          to="/"
+          className="font-medium text-indigo-500 hover:text-indigo-600 dark:hover:text-cyan-400"
+        >
+          Home
+        </Link>
+        <Link
+          to="/about"
+          className="font-medium text-indigo-500 hover:text-indigo-600 dark:hover:text-cyan-400"
+        >
+          About
+        </Link>
       </nav>
       <Outlet />
-    </>
+    </div>
   );
 }
--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 export function AboutPage() {
   return (
-    <main className="container">
-      <h1>About</h1>
-      <p>l8db mit TanStack Router.</p>
+    <main className="text-center mx-auto">
+      <h1 className="text-3xl font-semibold">About</h1>
+      <p className="mt-6">l8db mit TanStack Router.</p>
+      <Button className="mt-6">Click me</Button>
     </main>
   );
 }
